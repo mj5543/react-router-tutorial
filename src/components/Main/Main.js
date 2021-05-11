b99e3e10dc890336e4dceb3ce9e69a4c6357bc64
kangminju <a4475543@hotmail.com>
2021-05-11 10:15:13 +0900

    settings

---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+    
+    return (
+        <div className="main">
+            <h2>!Home!</h2>
+        </div>
+    );
+};
+
+export default Main;
